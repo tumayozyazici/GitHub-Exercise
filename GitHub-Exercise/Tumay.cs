4cf4e15677b7fd3573dddac83c189b70a7dd2464
tumay ozyazici <tumay.mm@gmail.com>
2024-05-24 14:11:07 +0300

    Add project files.

---
+++ GitHub-Exercise/Tumay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Exercise
+{
+	public class Tumay
+	{
+		//Burası Tümay'ın Branchi
+	}
+}
