13089a1fa54d794c3ae6c7019a642b5985dac084
Cemre Metin <cemremetindag@gmail.com>
2024-05-25 19:03:28 +0300

    age eklendi

--- GitHub-Exercise/Cemre.cs
+++ GitHub-Exercise/Cemre.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace GitHub_Exercise
 {
 	public class Cemre
 	{
-		//Burası Cemre'nin Branchi
-	}
+        //Burası Cemre'nin Branchi
+        public int Age { get; set; }
+        //Age property eklendi
+    }
 }
